1b366cf31c7c8c6e0a33cd4142b414b1a4ea3836
Ning An <ning.an@outlook.com>
2020-08-18 18:41:19 -0400

    Exercise 1

--- server/handlers.js
+++ server/handlers.js
@@ -1,4 +1,39 @@
-'use strict';
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+//create seats info
+const seats = [];
+const rows = ["A", "B", "C", "D", "E", "F", "G", "H"];
+const cols = 12;
+
+rows.forEach((row) => {
+  for (let i = 1; i <= cols; i++) {
+    seats.push({
+      _id: `${row}-${i}`,
+      price: 225,
+      isBooked: Math.random() > 0.5,
+    });
+  }
+});
+
+const createSeatsDB = async () => {
+  const client = await MongoClient(MONGO_URI, options);
+  await client.connect();
+
+  const db = client.db("workshop2");
+
+  await db.collection("seats").insertMany(seats);
+
+  client.close();
+};
+
+createSeatsDB();
 
 const getSeats = async (req, res) => {};
 
